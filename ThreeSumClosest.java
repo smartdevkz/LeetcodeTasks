a176e5eeef870f61c3f1418a938278f4371790e5
Ерлан Каракулов <smartdevkz@mail.ru>
2020-05-21 20:16:13 +0300

    just a simple approach: 6.67% beats

--- ThreeSumClosest.java
+++ ThreeSumClosest.java
@@ -1,7 +1,7 @@
 public class ThreeSumClosest {
     public static void main(String[] args) {
-        int[] nums = new int[] { 0, 2, 1, -3 };
-        int target = 1;
+        int[] nums = new int[] { -3, -2, -5, 3, -4 };
+        int target = -1;
         int res = threeSumClosest(nums, target);
         System.out.println(res);
     }
@@ -16,49 +16,22 @@ public class ThreeSumClosest {
             }
             return sum;
         }
-        insertSort(nums);
 
-        int center = nums.length-1;
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = nums[i] - target;
-            if (center == -1 && nums[i] >= 0) {
-                center = i;
+        int minDiff = Math.abs(nums[0] + nums[1] + nums[2] - target);
+        int sum = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int d = Math.abs(nums[i] + nums[j] + nums[k] - target);
+                    if (minDiff > d) {
+                        minDiff = d;
+                        sum = nums[i] + nums[j] + nums[k];
+                    }
+                }
+
             }
         }
-
-        int start = center >= 3 ? center - 3 : 0;
-        int end = (center + 2) < nums.length ? (center + 2) : nums.length - 1;
-        return findSum(nums, start, end) + 3 * target;
+        return sum;
     }
 
-    public static int findSum(int[] nums, int start, int end) {
-        Integer min = null;
-        for (int i = start; i <= end - 2; i++) {
-            int sum = nums[i] + nums[i + 1] + nums[i + 2];
-            if (min == null) {
-                min = sum;
-            } else {
-                if (Math.abs(min) > Math.abs(sum))
-                    min = sum;
-            }
-        }
-        return min;
-    }
-
-    public static void insertSort(int[] nums) {
-        int n = nums.length;
-        for (int k = 1; k < n; k++) {
-            int cur = nums[k];
-            int j = k;
-            while (j > 0 && nums[j - 1] > cur) {
-                nums[j] = nums[j - 1];
-                j--;
-            }
-            nums[j] = cur;
-        }
-    }
-
-    static void println(Object x) {
-        System.out.println(x);
-    }
 }
