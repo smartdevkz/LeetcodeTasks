e1ba12b379946d1dd476122bd1040ed90efb937b
Ерлан Каракулов <smartdevkz@mail.ru>
2020-06-05 14:30:58 +0300

    Implement strStr(): solution

--- TwoPointer/ImplementStrStrApp.java
+++ TwoPointer/ImplementStrStrApp.java
@@ -7,22 +7,12 @@ public class ImplementStrStrApp {
     }
 
     public int strStr(String haystack, String needle) {
-        if(needle.length()==0) return 0;
-        if (haystack.length() < needle.length())return -1;
-        if(haystack.equals(needle))return 0;
-
-        int k = 0;
-        int i = 0;
-        while (i<(haystack.length()-needle.length()+k)) {
-            if (haystack.charAt(i) == needle.charAt(k)) {
-                if (k == (needle.length() - 1)) return i - k;
-                k++;
-                i++;
-            } else {
-                i = i-k+1;
-                k = 0;
+        for (int i = 0;; i++) {
+            for (int j = 0;; j++) {
+                if (j == needle.length()) return i;
+                if (i + j == haystack.length()) return -1;
+                if (needle.charAt(j) != haystack.charAt(i + j))break;
             }
         }
-        return -1;
     }
 }
