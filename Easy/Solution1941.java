27e58795e7911393e6f364554c771090862bd632
Yerlan Karakulov <smartdevkz@mail.ru>
2021-10-16 22:42:02 +0600

    1941. Check if All Characters Have Equal Number of Occurrences: r-100%, m-82%

--- Easy/Solution1941.java
+++ Easy/Solution1941.java
@@ -5,19 +5,18 @@ import java.util.HashMap;
 //1941. Check if All Characters Have Equal Number of Occurrences
 public class Solution1941 {
     public boolean areOccurrencesEqual(String s) {
-        HashMap<Character, Integer> hm = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            Character ch = s.charAt(i);
-            int count = hm.containsKey(ch) ? hm.get(ch) : 0;
-            hm.put(ch, count + 1);
+        int[] freq = new int[26];
+
+        for (var c : s.toCharArray()) {
+            freq[c - 'a']++;
         }
 
-        int count = hm.get(s.charAt(0));
-        for (var key : hm.keySet()) {
-            if (count != hm.get(key)) {
+        int prev = freq[s.charAt(0) - 'a'];
+        for (int i : freq) {
+            if (i > 0 && i != prev)
                 return false;
-            }
         }
+
         return true;
     }
 }
