c1c16a726259548c236f64b92b9d44c95e064f66
smartdevkz <ereke_enu@mail.ru>
2021-10-20 20:49:53 +0300

    496. Next Greater Element I: r-67%, m-81%

--- Easy/Solution496.java
+++ Easy/Solution496.java
@@ -3,13 +3,12 @@ package Easy;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 
 //496. Next Greater Element I
 public class Solution496 {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         HashMap<Integer, Integer> hm = getHm(nums2);
-        for (int i = 0; i < nums1.length; i++) {
+        for(int i=0;i<nums1.length;i++){
             nums1[i] = hm.getOrDefault(nums1[i], -1);
         }
         return nums1;
@@ -18,14 +17,16 @@ public class Solution496 {
     HashMap<Integer, Integer> getHm(int[] nums) {
         HashMap<Integer, Integer> hm = new HashMap<>();
         HashSet<Integer> hs = new HashSet<>();
+        
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i] < nums[i + 1]) {
                 hm.put(nums[i], nums[i + 1]);
-                while (hs.iterator().hasNext()) {
-                    int number = hs.iterator().next();
+                var iterator = hs.iterator();
+                while (iterator.hasNext()) {
+                    int number = iterator.next();
                     if (number < nums[i + 1]) {
                         hm.put(number, nums[i + 1]);
-                        hs.remove(number);
+                        iterator.remove();
                     }
                 }
             } else {
