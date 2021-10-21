5a57333aa46de14901eb5723dfc08c1fc5b8d485
smartdevkz <ereke_enu@mail.ru>
2021-10-21 20:37:21 +0300

    709. To Lower Case implementation

--- Easy/Solution709.java
+++ Easy/Solution709.java
@@ -2,7 +2,19 @@ package Easy;
 
 //709. To Lower Case
 public class Solution709 {
+    public static void main(String[] args) {
+
+    }
+
     public String toLowerCase(String s) {
-        return s.toLowerCase();    
+        StringBuilder builder = new StringBuilder();
+        for (Character ch : s.toCharArray()) {
+            if (ch > 'z') {
+                builder.append((char) ch + 32);
+            } else {
+                builder.append(ch);
+            }
+        }
+        return builder.toString();
     }
 }
