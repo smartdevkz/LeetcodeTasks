678561682b210522795083725deea675db8b6675
smartdevkz <ereke_enu@mail.ru>
2021-10-21 18:50:34 +0300

    437. Path Sum III

---
+++ Easy/Solution437.java
@@ -0,0 +1,28 @@
+package Easy;
+
+//437. Path Sum III
+public class Solution437 {
+    public int pathSum(TreeNode root, int targetSum) {
+        int k = 0;
+        return k;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
