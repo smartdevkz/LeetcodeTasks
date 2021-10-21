12ef0a099d21ae7f7dcf61bb8c0b95956f624d93
smartdevkz <ereke_enu@mail.ru>
2021-10-21 20:15:19 +0300

    1662. Check If Two String Arrays are Equivalent: Approach with custom iterator was ineffective. r-8%, m-35%

--- Easy/Solution1662.java
+++ Easy/Solution1662.java
@@ -2,15 +2,51 @@ package Easy;
 
 //1662. Check If Two String Arrays are Equivalent
 public class Solution1662 {
+    public static void main(String[] args) {
+        var word1 = new String[] { "ab", "c" };
+        var word2 = new String[] { "a", "bc" };
+        var app = new Solution1662();
+        var res = app.arrayStringsAreEqual(word1, word2);
+        System.out.println(res);
+    }
+
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        StringBuilder builder1 = new StringBuilder();
-        StringBuilder builder2 = new StringBuilder();
-        for (var item : word1) {
-            builder1.append(item);
+        var iterator1 = new WordIterator(word1);
+        var iterator2 = new WordIterator(word2);
+        while (iterator1.hasNext() && iterator2.hasNext()) {
+            var ch1 = iterator1.next();
+            var ch2 = iterator2.next();
+            if (ch1 != ch2) {
+                return false;
+            }
         }
-        for (var item : word2) {
-            builder2.append(item);
+        return !iterator1.hasNext() && !iterator2.hasNext();
+    }
+
+    class WordIterator {
+        String[] words;
+        int i = 0;
+        int j = -1;
+
+        public WordIterator(String[] words) {
+            this.words = words;
         }
-        return builder1.toString().equals(builder2.toString());
+
+        public boolean hasNext() {
+            if (j + 1 < words[i].length()) {
+                return true;
+            } else {
+                return i < words.length - 1;
+            }
+        }
+
+        public Character next() {
+            j++;
+            if (j >= words[i].length()) {
+                j = 0;
+                i++;
+            }
+            return i < words.length ? words[i].charAt(j) : null;
+        }
     }
 }
