2d96992b93b85767b425c37c4c059fa2942545b4
smartdevkz <ereke_enu@mail.ru>
2021-10-24 22:38:14 +0300

    2042. Check if Numbers Are Ascending in a Sentence: 58/56

---
+++ Easy/Solution2042.java
@@ -0,0 +1,20 @@
+package Easy;
+
+import utils.Utils;
+
+//2042. Check if Numbers Are Ascending in a Sentence
+public class Solution2042 {
+    public static void main(String[] args) {
+        var app = new Solution2042();
+        Utils.print(app.areNumbersAscending("hello world 5 x 5"));
+    }
+
+    public boolean areNumbersAscending(String s) {
+        var prev = -1;
+        for (var item : s.split(" ")) {
+            if (!Character.isDigit(item.charAt(0)))
+                continue;
+            int number = Integer.parseInt(item);
+        }
+    }
+}
