02de89ee888a7308f90277ec6e464e66a6fece00
smartdevkz <ereke_enu@mail.ru>
2021-10-29 20:17:50 +0300

    1480. Running Sum of 1d Array: 100/60

---
+++ Easy/Solution1480.java
@@ -0,0 +1,11 @@
+package Easy;
+
+//1480. Running Sum of 1d Array
+public class Solution1480 {
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+        return nums;
+    }
+}
