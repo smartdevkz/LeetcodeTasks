e6e0120596ebb7924b70eefcbc01a4f1ec9126b3
smartdevkz <ereke_enu@mail.ru>
2021-12-11 20:29:58 +0300

    https://leetcode.com/problems/count-sorted-vowel-strings/ 100/91

---
+++ Medium/Solution1641.java
@@ -0,0 +1,11 @@
+package Medium;
+
+//https://leetcode.com/problems/count-sorted-vowel-strings/
+public class Solution1641 {
+    public int countVowelStrings(int n) {
+        Character[] vowels = new Character[] { 'a', 'e', 'i', 'o', 'u' };
+
+        return 0;
+    }
+}
+    
