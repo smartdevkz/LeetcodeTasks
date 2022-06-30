7965393696e789850ddebfc79a2b129f7d95e5fd
Yerlan Karakulov <smartdevkz@mail.ru>
2022-06-30 12:40:30 +0600

    42-trapping water

--- TopLeetcode/src/Hard/Solution42.java
+++ TopLeetcode/src/Hard/Solution42.java
@@ -1,29 +1,29 @@
 package Hard;
 
 public class Solution42 {
-    public int trap(int[] height) {
-        int h = 0, d = 0, sum = 0;
-        for (int i = 0; i < height.length; i++) {
-            if (height[i] < h) {
-                d += h - height[i];
+    public int trap(int[] heights) {
+        int left = 0;
+        int right = heights.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int result = 0;
+        while (left < right) {
+            if (heights[left] < heights[right]) {
+                if (leftMax <= heights[left]) {
+                    leftMax = heights[left];
+                } else {
+                    result += leftMax - heights[left];
+                }
+                left++;
             } else {
-                h = height[i];
-                sum += d;
-                d = 0;
+                if (rightMax <= heights[right]) {
+                    rightMax = heights[right];
+                } else {
+                    result += rightMax - heights[right];
+                }
+                right--;
             }
         }
-        d = 0;
-        h = 0;
-        for (int i = height.length - 1; i >= 0; i--) {
-            if (height[i] < h) {
-                d += h - height[i];
-            } else {
-                h = height[i];
-                sum += d;
-                d = 0;
-            }
-        }
-        return sum;
+        return result;
     }
 
     public static void main(String[] args) {
