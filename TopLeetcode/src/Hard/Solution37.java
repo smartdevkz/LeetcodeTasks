63852c6e539ca65ea1be27876f8403211411a3bf
Yerlan Karakulov <smartdevkz@mail.ru>
2022-07-21 16:49:35 +0600

    37. Sudoku solver. Solution: tc O(1), sc O(1) because size of params is fixed

--- TopLeetcode/src/Hard/Solution37.java
+++ TopLeetcode/src/Hard/Solution37.java
@@ -4,69 +4,86 @@ import java.util.*;
 
 //https://leetcode.com/problems/sudoku-solver/
 public class Solution37 {
-    int count = 0;
+
+    int N = 9;
+
+    char[][] board;
+    int[][] rows;
+    int[][] cols;
+    int[][] boxes;
+    boolean sudokuSolved = false;
 
     public void solveSudoku(char[][] board) {
-        HashMap<Integer, List<Integer>> rows = new HashMap<>();
-        HashMap<Integer, List<Integer>> cols = new HashMap<>();
-        for (int i = 0; i < 9; i++) {
-            var lst1 = new ArrayList<Integer>();
-            var lst2 = new ArrayList<Integer>();
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') {
-                    board[i][j] = '0';
-                    count++;
-                }
-                int n1 = Character.getNumericValue(board[i][j]);
-                lst1.add(n1);
-                if (board[j][i] == '.') {
-                    board[j][i] = '0';
-                    count++;
-                }
-                int n2 = Character.getNumericValue(board[j][i]);
-                lst2.add(n2);
-            }
-            rows.put(i, lst1);
-            cols.put(i, lst2);
-        }
+        initArrays(board);
+        backtrack(0, 0);
+    }
 
-        int k = 0;
-        while (count > 0) {
-            fill(k, rows, cols, board);
-            k++;
-            if (k == 9) k = 0;
+    void initArrays(char[][] board) {
+        this.board = board;
+        rows = new int[N][N + 1];
+        cols = new int[N][N + 1];
+        boxes = new int[N][N + 1];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (board[i][j] != '.') {
+                    int d = Character.getNumericValue(board[i][j]);
+                    rows[i][d]++;
+                    cols[j][d]++;
+                    var boxIdx = getBoxNum(i, j);
+                    boxes[boxIdx][d]++;
+                }
+            }
         }
     }
 
-    void fill(int k, HashMap<Integer, List<Integer>> rows, HashMap<Integer, List<Integer>> cols, char[][] board) {
-        int rowIdx = 3 * (k / 3);
-        int colIdx = 3 * (k % 3);
-        var possible = new HashSet<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
-        for (int i = rowIdx; i < rowIdx + 3; i++) {
-            for (int j = colIdx; j < colIdx + 3; j++) {
-                int n = Character.getNumericValue(board[i][j]);
-                possible.remove(n);
-            }
-        }
-        for (int i = rowIdx; i < rowIdx + 3; i++) {
-            for (int j = colIdx; j < colIdx + 3; j++) {
-                if (board[i][j] != '0') continue;
-                var options = new HashSet<>(possible);
-                var rowValues = rows.get(i);
-                for (var n : rowValues) {
-                    options.remove(n);
+    int getBoxNum(int row, int col) {
+        return 3 * (row / 3) + col / 3;
+    }
+
+    void backtrack(int row, int col) {
+        if (board[row][col] == '.') {
+            for (int d = 1; d < 10; d++) {
+                if (couldPlace(d, row, col)) {
+                    placeNumber(d, row, col);
+                    placeNextNumbers(row, col);
+                    if (!sudokuSolved) {
+                        removeNumber(d, row, col);
+                    }
                 }
-                var colValues = cols.get(j);
-                for (var n : colValues) {
-                    options.remove(n);
-                }
-                if (options.size() != 1) continue;
-                var value = (int) options.toArray()[0];
-                board[i][j] = (char) value;
-                rowValues.set(j, value);
-                colValues.set(i, value);
-                count--;
             }
+        } else {
+            placeNextNumbers(row, col);
         }
     }
+
+    boolean couldPlace(int d, int row, int col) {
+        int idx = getBoxNum(row, col);
+        return rows[row][d] + cols[col][d] + boxes[idx][d] == 0;
+    }
+
+    void placeNumber(int d, int row, int col) {
+        rows[row][d]++;
+        cols[col][d]++;
+        int idx = getBoxNum(row, col);
+        boxes[idx][d]++;
+        board[row][col] = (char) (d + '0');
+    }
+
+    void removeNumber(int d, int row, int col) {
+        rows[row][d]--;
+        cols[col][d]--;
+        int idx = getBoxNum(row, col);
+        boxes[idx][d]--;
+        board[row][col] = '.';
+    }
+
+    void placeNextNumbers(int row, int col) {
+        if (row == N - 1 && col == N - 1) {
+            sudokuSolved = true;
+            return;
+        }
+        if (col == N - 1) backtrack(row + 1, 0);
+        else backtrack(row, col + 1);
+    }
+
 }
