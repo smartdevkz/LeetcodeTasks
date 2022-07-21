4b4b24bd2c2c0f4a4329755f48d4038bda5715c3
Yerlan Karakulov <smartdevkz@mail.ru>
2022-07-21 17:29:05 +0600

    39. Combinations. Solved, but could not calculate tc, sc

---
+++ TopLeetcode/src/Medium/Solution39.java
@@ -0,0 +1,36 @@
+package Medium;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+//https://leetcode.com/problems/combination-sum/
+class Solution39 {
+    List<List<Integer>> result;
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        result = new ArrayList<List<Integer>>();
+        backtrack(0, target, new LinkedList<>(), candidates);
+        return result;
+    }
+
+    void backtrack(int idx, int n, LinkedList<Integer> lst, int[] candidates) {
+        if (n == 0) {
+            result.add(new ArrayList<>(lst));
+            return;
+        }
+        if (n < 0) return;
+        for (int i = idx; i < candidates.length; i++) {
+            int c = candidates[i];
+            int diff = n - c;
+            lst.add(c);
+            backtrack(i, diff, lst, candidates);
+            lst.removeLast();
+        }
+    }
+
+    public static void main(String[] args) {
+        var app = new Solution39();
+        var res = app.combinationSum(new int[]{2, 3, 6, 7}, 7);
+    }
+}
